d58430c59e25d940dbd009bfab5063ffa864069c
Tyler Guerrero <guerrero.tyler3@gmail.com>
2019-07-29 00:24:42 -0400

    new flame

--- maptest/src/App.js
+++ maptest/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState }from 'react';
 import ReactMapGL from 'react-map-gl';
-import MapGL, {GeolocateControl} from 'react-map-gl'
+import MapGL, {GeolocateControl} from 'react-map-gl';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl';
 // For the current.. this sets the poistion on the screen for them.
+
 const geolocateStyle = {
   position: 'absolute',
   top: 0,
@@ -9,6 +11,39 @@ const geolocateStyle = {
   margin: 10
 };
 
+mapboxgl.accessToken = 'pk.eyJ1IjoidGd1ZXJyMTIiLCJhIjoiY2p5Y2J0YXk5MGhodjNtcGViMmtkNnN0YiJ9.y0hb7dOaAeimxUfx93rcyA'; // replace this with your access token
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/tguerr12/cjyd6kex42rps1cmomr9a7sz0', // replace this with your style URL
+      center: [-81.2000599,28.6024274],
+      zoom: 10.7
+    });
+    map.addControl(new mapboxgl.GeolocateControl({
+      positionOptions: {
+      enableHighAccuracy: true
+      },
+      trackUserLocation: true
+      }));
+    
+    map.on('click', function(e) {
+    var features = map.queryRenderedFeatures(e.point, {
+    layers: ['orlando-running-spots'] // replace this with the name of the layer
+  });
+
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var popup = new mapboxgl.Popup({ offset: [0, -15] })
+    .setLngLat(feature.geometry.coordinates)
+    .setHTML('<h3>' + feature.properties.title + '</h3><p>' + feature.properties.description + '</p>')
+    .addTo(map);
+});
+
+
+//
 // For this export it will set the size of the map and what coordinates it will spawn the map on
   export default function App() {
     const [viewport, setViewport] = useState({
@@ -16,13 +51,11 @@ const geolocateStyle = {
       longitude: -81.2000599,
       width: "100vw",
       height: "100vh",
-      zoom: 10 ,
+      zoom: 10,
     });
-  
-    
+
     
   return (
-    <div>
       <ReactMapGL 
         {...viewport} 
         mapboxApiAccessToken='pk.eyJ1IjoidGd1ZXJyMTIiLCJhIjoiY2p5Y2J0YXk5MGhodjNtcGViMmtkNnN0YiJ9.y0hb7dOaAeimxUfx93rcyA'
@@ -36,11 +69,9 @@ const geolocateStyle = {
           positionOptions={{enableHighAccuracy: true}}
           trackUserLocation={true}
         />
+        
 
       </ReactMapGL>
-
-      
-      
-    </div>
+  
   );
 }
